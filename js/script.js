a4470482cbf04e3c1a43bf4c8531012710e9a988
あなたの名前 <20akanek@gmail.com>
2021-06-14 21:00:32 +0900

    ページトップリンク

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
-$('#wrap').scrollgress({//バーの高さの基準となるエリア指定
-    height: '5px',//バーの高さ
-    color: '#990000',//バーの色
+$('#page-top').click(function () {//バーの高さの基準となるエリア指定
+    $('body,html').animate({
+        scrollTop: 0
+    }, 500);
+    return false;
 });
